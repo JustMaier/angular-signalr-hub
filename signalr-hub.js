96899ce49555ae00f46f42fbea702c21a1a8ebb5
evanharris <evan.tyler.harris@gmail.com>
2014-06-11 22:31:30 -0700

    adding ability to define root signalr url

--- signalr-hub.js
+++ signalr-hub.js
@@ -1,36 +1,48 @@
 angular.module('SignalR', [])
-.constant('$', $)
-.factory('Hub', ['$', function ($) {
-	//This will allow same connection to be used for all Hubs
-	//It also keeps connection as singleton.
-	var globalConnection = $.hubConnection();
-	return function (hubName, listeners, methods) {
-		var Hub = this;
-		Hub.connection = globalConnection;
-		Hub.proxy = Hub.connection.createHubProxy(hubName);
-		Hub.on = function (event, fn) {
-			Hub.proxy.on(event, fn);
-		};
-		Hub.invoke = function (method, args) {
-			return Hub.proxy.invoke.apply(Hub.proxy, arguments)
-		};
+  .constant('$', $)
+  .factory('Hub', ['$', function ($) {
+    //This will allow same connection to be used for all Hubs
+    //It also keeps connection as singleton.
+    var globalConnection = null;
 
-		if (listeners) {
-			angular.forEach(listeners, function (fn, event) {
-				Hub.on(event, fn);
-			});
-		}
-		if (methods) {
-			angular.forEach(methods, function (method) {
-				Hub[method] = function () {
-					var args = $.makeArray(arguments);
-					args.unshift(method);
-					return Hub.invoke.apply(Hub, args);
-				};
-			});
-		}
-		//Adding additional property of promise allows to access it in rest of the application.
-		Hub.promise = Hub.connection.start();		
-		return Hub;
-	};
-}]);
+    var initGlobalConnection = function (rootPath) {
+      if (rootPath) {
+        globalConnection = $.hubConnection(rootPath, {userDefaultPath: false});
+      } else {
+        globalConnection = $.hubConnection();
+      }
+    };
+
+    return function (hubName, listeners, methods, rootPath) {
+      var Hub = this;
+      if (globalConnection === null) {
+        initGlobalConnection(rootPath);
+      }
+      Hub.connection = globalConnection;
+      Hub.proxy = Hub.connection.createHubProxy(hubName);
+      Hub.on = function (event, fn) {
+        Hub.proxy.on(event, fn);
+      };
+      Hub.invoke = function (method, args) {
+        return Hub.proxy.invoke.apply(Hub.proxy, arguments)
+      };
+
+      if (listeners) {
+        angular.forEach(listeners, function (fn, event) {
+          Hub.on(event, fn);
+        });
+      }
+      if (methods) {
+        angular.forEach(methods, function (method) {
+          Hub[method] = function () {
+            var args = $.makeArray(arguments);
+            args.unshift(method);
+            return Hub.invoke.apply(Hub, args);
+          };
+        });
+      }
+      //Adding additional property of promise allows to access it in rest of the application.
+      Hub.promise = Hub.connection.start();
+      return Hub;
+    };
+  }]);
